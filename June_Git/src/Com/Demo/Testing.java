f70e319a0289048926382c3e169b04d6507cc0b0
vansh <pawarsmita12345678@gmail.com>
2023-06-24 10:57:24 +0530

    By smita

---
+++ June_Git/src/Com/Demo/Testing.java
@@ -0,0 +1,25 @@
+package Com.Demo;
+
+public class Testing 
+{
+	public void m1()
+	{
+		System.out.println("Smita-----Hello");
+		
+	}
+	
+	public void m2()
+	{
+		System.out.println("Pawar-----Hello");
+		
+	}
+	
+
+	public static void main(String[] args)
+	{
+		Testing t=new Testing();
+		t.m1();
+		t.m2();
+	}
+
+}
